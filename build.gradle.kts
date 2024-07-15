e89768637f2b8564d4165dceabe75a67f011e915
g000sha256 <detmmpmznb@g000sha256.dev>
2024-07-15 20:04:35 +0300

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 val libraryGroup = "dev.g000sha256"
 val libraryModule = "sonatype-maven-central"
-val libraryVersion = "0.0.1"
+val libraryVersion = "0.0.2"
 
 group = libraryGroup
 version = libraryVersion
