c02df1461792cf8fadd25291cd53e98118b77e9e
Georgii Ippolitov <github@g000sha256.dev>
2026-05-02 10:25:03 +0300

    Update copyright

--- src/main/kotlin/g000sha256/sonatype_maven_central/internal/util/Base64.kt
+++ src/main/kotlin/g000sha256/sonatype_maven_central/internal/util/Base64.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Georgii Ippolitov (g000sha256)
+ * Copyright 2024-2026 Georgii Ippolitov (g000sha256)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
